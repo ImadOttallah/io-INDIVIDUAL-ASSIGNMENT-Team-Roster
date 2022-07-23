a640f21688d5ba8944365a0e39d6d541466a3e69
Imad Ottallah <imadotta@gmail.com>
2022-07-23 09:05:47 -0500

    working on netlify

---
+++ api/mergedData.js
@@ -0,0 +1,6 @@
+// import { getSinglePlayer } from "./playerData"
+
+// const viewPlayerDetails = (playerFireBaseKey) => new Promise((resolve, reject) => {
+//   Promise.all([getSinglePlayer(playerFireBaseKey)]).then((playerObject) => {
+//   })
+// }
